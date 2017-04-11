b264a6d15bda6d376855d957281f40b0fe08b2e4
Yongzheng Huang <iamhuangyz@gmail.com>
2017-04-11 12:29:56 -0500

    added word cloud to map section

--- website/js/main.js
+++ website/js/main.js
@@ -5,11 +5,20 @@ $(function() {
     var tweetsByStatesURL = "tweets_states/"
 
     //Create a new instance of the word cloud visualisation.
-    var myWordCloud = wordCloud('#wordcloud-vis', 1000, 500);
+    var smallWordCloud = wordCloud('#small-wordcloud-vis', 300, 300);
 
     // Query tweets and render hashtag word cloud.
     $.get( baseURL + tweetsByStatesURL, { date: "2017-03-16"} ).done(function( data ) {
-        myWordCloud.update(JSON.parse(data)[0]['hashtags']);
+        smallWordCloud.update(JSON.parse(data)[0]['hashtags']);
+        console.log( JSON.parse(data)[0]['hashtags'] );
+    });
+
+    //Create a new instance of the word cloud visualisation.
+    var largeWordCloud = wordCloud('#wordcloud-vis', 1000, 300);
+
+    // Query tweets and render hashtag word cloud.
+    $.get( baseURL + tweetsByStatesURL, { date: "2017-03-16"} ).done(function( data ) {
+        largeWordCloud.update(JSON.parse(data)[0]['hashtags']);
         console.log( JSON.parse(data)[0]['hashtags'] );
     });
 });
