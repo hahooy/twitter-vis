cd7163e8d0b63489218029133b3162f890245388
Yongzheng Huang <iamhuangyz@gmail.com>
2017-04-11 22:19:27 -0500

    slider working

--- website/js/main.js
+++ website/js/main.js
@@ -2,26 +2,28 @@ $(function() {
     // Constants.
     var baseURL = "http://127.0.0.1:8000/twitter_vis/";
     //var baseURL = "http://ec2-54-91-157-7.compute-1.amazonaws.com:8000/twitter_vis/";
-    var tweetsByStatesURL = "tweets_states/"
-    var allHashtagURL = "top_hashtags/"
+    var tweetsByStatesURL = "tweets_states/";
+    var allHashtagURL = "top_hashtags/";
 
     //Create a new instance of the word cloud visualisation.
-    var smallWordCloud = wordCloud('#small-wordcloud-vis', 300, 300);
+    var smallWordCloud = wordCloud('#small-wordcloud-vis', 300, 500);
     var largeWordCloud = wordCloud('#wordcloud-vis', 1000, 300);
-
-    // Query parameters.
-    var params = {
-        date: "2017-03-16"
-    };
 
     // Create a gis map.
     var gisMap = generateMap();
+
+    // Query parameters.
+    var params = {
+        date: "2017-03-13"
+    };
+
+    
 
     // Query wrapper.
     function queryTweets(params) {
         // Query data and render.
         $.get( baseURL + tweetsByStatesURL, params ).done(function( data ) {
-            var data = JSON.parse(data);
+            data = JSON.parse(data);
             console.log(data);
             renderData(data);
         });
@@ -29,8 +31,8 @@ $(function() {
 
     // make a new request after the user select a hashtag.
     function renderData(data) {
-        smallWordCloud.update(data[0]['hashtags']);
-        largeWordCloud.update(data[0]['hashtags']);
+        smallWordCloud.update(data[0].hashtags);
+        largeWordCloud.update(data[0].hashtags);
         insertRadius(data);
         gisMap.bubbles(data);
     }
@@ -60,7 +62,7 @@ $(function() {
         });
 
         // Slider for choosing a date.
-        $("#slider-5").slider({
+        $("#slider").slider({
             orientation: "horizontal",
             value: 13,
             max: 19,
@@ -69,11 +71,12 @@ $(function() {
               "ui-slider": "highlight"
             },
             slide: function (event, ui) {
-              $("#minval").val(ui.value);
-              //generateMap(ui.value);
+                $("#minval").val(ui.value);
+              params.date = "2017-03-" + ui.value;
+              queryTweets(params);
             }
           });
-        $("#minval").val($("#slider-5").slider("value"));
+        $("#minval").val($("#slider").slider("value"));
 
         queryTweets(params);
     }
